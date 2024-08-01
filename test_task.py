274a5884366500c3bedd8cae0e1434f126c25267
Mei <mei.hao@yale.edu>
2024-08-01 15:47:08 -0400

    Playing with the env

--- test_task.py
+++ test_task.py
@@ -1,5 +1,10 @@
 import manipulator_learning.sim.envs as manlearn_envs
-env = getattr(manlearn_envs, 'PandaPlayInsertTrayXYZState')()
+from icecream import ic
+
+
+env = getattr(manlearn_envs, 'ThingPickAndInsertSucDoneImage')()
 
 obs = env.reset()
 next_obs, rew, done, info = env.step(env.action_space.sample())
+
+ic(next_obs)
